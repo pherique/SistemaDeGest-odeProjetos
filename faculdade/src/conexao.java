14af7d5bd4a5bc25e937719b8f5550c891c547c3
Pedro Henrique Silva Duarte <phenrique.sduarte@gmail.com>
2025-09-04 21:28:14 -0300

    novos arquivos

--- faculdade/src/conexao.java
+++ faculdade/src/conexao.java
@@ -1,9 +1,19 @@
-public class conexao {
-    public static void main (String[] args) {
-        try{
-            Class.forName("com.mysql.jdc.Driver") ;
-        } catch (ClassNotFoundException ex) {
-            System.out.println("Driver do banco de dados não localizado");
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConexaoMySQL {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/sistemagestao";
+        String usuario = "root";
+        String senha = "Carpedcc123";
+
+        try {
+            Connection conexao = DriverManager.getConnection(url, usuario, senha);
+            System.out.println("Conexão realizada com sucesso!");
+            conexao.close();
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar: " + e.getMessage());
         }
     }
 }
